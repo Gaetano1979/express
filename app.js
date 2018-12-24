a6f9677c37a141dba89f7e61b6d77f8a394f371e
Gaetano Sabino <gaetano.sabino@gmail.com>
2018-12-24 11:30:25 -0500

    prova

--- app.js
+++ app.js
@@ -2,20 +2,75 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { MysqlClass } = require('./mysql/mysql');
+let query = `
+    select idcliente,cliente from clientes`;
+let query1 = `
+describe clientes`;
+let query2 = `
+ SHOW TABLES;
+`;
+let pagos = `select * from pagos`;
+let rel = `
+SELECT clientes.cliente, pagos.idcliente FROM pagos JOIN pagos_clientes
+ON (clientes.idclientes = pagos_clientes.cliente) JOIN pagos
+ON (pagos.idpago = pagos_cliente.pagos)`;
+let pro = `
+select clientes.idcliente,pagos.idcliente from pagos where idcliente=pagos.idcliente`;
+
+
 
 
 
 app.get('/', (req, res) => {
-    // res.send('Hello Word');
-    res.json({
-        ok: true,
-        mensaje: 'todo bien en Heroku'
+    MysqlClass.conessione();
+    MysqlClass.resultado(query, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.send(err)
+        } else {
+            // console.log(res);
+            res.json({
+                data
+            });
+        }
     });
+    MysqlClass.chiudere();
     console.log('todo bien');
+});
+app.get('/pagos', (req, res) => {
+    MysqlClass.conessione();
+    MysqlClass.resultado(pagos, (err, pagos) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(query);
+            console.log(res.json());
 
+            // res.json({
+            //     ok: true,
+            //     data: pagos
+            // });
+        }
+    });
+    MysqlClass.chiudere();
 });
 app.get('/clientes', (req, res) => {
-    res.send('Bienvenidos a clientes');
+    // res.send('Bienvenidos a clientes');
+    MysqlClass.resultado(query, (err, resul) => {
+        if (err) {
+            console.log(err);
+
+        } else {
+            res.json({
+                ok: true,
+                data: resul
+            });
+
+
+        }
+
+    });
 });
 
 app.listen(port, () => {
---
+++ mysql/mysql.js
@@ -0,0 +1,66 @@
+const Mysql = require('mysql');
+
+let conessione;
+
+
+
+let MysqlClass = {
+    hostClass: 'ls-4ced6077d7519bffb5de949e2bae2dcbe3615210.c5s20s1vsvjo.eu-central-1.rds.amazonaws.com',
+    password: 'gaetano1979',
+    user: 'dbmasteruser',
+    database: 'sistematic_net',
+    conessione: () => {
+        conessione = Mysql.createConnection({
+            host: MysqlClass.hostClass,
+            password: MysqlClass.password,
+            user: MysqlClass.user,
+            database: MysqlClass.database
+        });
+        conessione.connect((err) => {
+            if (err) {
+                console.log(err);
+
+
+            } else {
+                console.log('Conessione eseguita con successo');
+            }
+        });
+    },
+    chiudere: () => {
+        conessione.end((err) => {
+            if (err) {
+                console.log(err);
+
+            } else {
+                console.log('Conessione Chiusa');
+
+            }
+        })
+    },
+    resultado: (queryresultado, callback) => {
+        let conessione = Mysql.createConnection({
+            host: MysqlClass.hostClass,
+            password: MysqlClass.password,
+            user: MysqlClass.user,
+            database: MysqlClass.database
+        });
+        conessione.query(queryresultado, (err, resultado) => {
+            console.log(queryresultado);
+            if (err) {
+                console.log(err);
+                return callback(err);
+            }
+            if (resultado.length === 0) {
+                return callback('Registro no encontrado');
+
+            } else {
+                console.log('resultati', resultado);
+                return callback(null, resultado);
+            }
+        });
+
+    }
+
+
+};
+module.exports = { MysqlClass };
